e4c4183746e28d3a387fb2950741bf3e3e5c2ef4
Thiago Borges <thandbor@gmail.com>
2025-06-30 14:42:30 -0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Gerador de Carteiras Bitcoin Testenet
+Programa que gera no terminal uma carteira bitcoin,
+utilizando como base: a Bip39 e Bip32.
+
+
+- node V22.14.0
---
+++ src/main.js
@@ -0,0 +1,20 @@
+const bitcoin = require('bitcoinjs-lib')
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+
+const network = bitcoin.networks.testnet
+const path = `m/49'/1'/0'/0`
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+let root = bip32.fromSeed(seed, network)
+
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network
+}).address
+
+console.log("Carteira gerada\nEndereço: ", btcAddress)
+console.log("Chave privada: ",node.toWIF())
+console.log("Seed: ", mnemonic)
